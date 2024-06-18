8aff07d5f1b1e7b6c7668f6f2a7505eee0ae02a1
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-06-18 00:22:45 -0300

    Terminando de resolver os exercícios

--- numero-maior-e-menor.py
+++ numero-maior-e-menor.py
@@ -7,3 +7,17 @@
 
 #Bônus2: peça para o usuário 3 números e diga o mais alto.
 
+
+print("Olá! Vamos adivinhar qual é o número maior?")
+
+num1 = int(input("Digite aqui o primeiro número: "))
+num2 = int(input("Digite aqui o segundo número: "))
+
+if num1 > num2:
+    print(f"O número {num1} é maior que o {num2}")
+elif num2 == num1:
+    print(f"Os números {num1} e {num2} são iguais !")
+else:
+    print(f"O número {num2} é maior que o número {num1}")
+
+
