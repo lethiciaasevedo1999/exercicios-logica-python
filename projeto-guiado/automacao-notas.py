812930071f2e138b18a68e0af8ee2803b676e1d7
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-02 00:00:58 -0300

    Criando laço de repetição para definir quantas notas o usuário irá inserir

---
+++ projeto-guiado/automacao-notas.py
@@ -0,0 +1,27 @@
+dataset = {}
+nome = ""
+
+def main():
+    print("\n--- Seja bem vinda a Escola do Reprograma! ---\n")
+    print("Aqui voê pode calcular a aprovação de uma aluna.\n")
+
+    obter_dados_aluna()
+
+def obter_dados_aluna():
+    print("Insira os dados: ")
+    nome = input("Nome da aluna: ")
+    turma = input("Turma da aluna: ")
+
+    notas = obter_notas()
+
+def obter_notas():
+    quantidade_notas = input("Quantidade de notas: ")
+    notas =[] #Lista para receber as notas
+    #É recomendado utilizar o "for" quando tivermos uma contagem pré definida.
+    #Exemplo: eu sei que a minha contagem vai de 0 1 20
+    for i in range(int(quantidade_notas)):
+        entrada = input(f"Insira sua nota{i + 1}: ")
+        nota = float(entrada)
+        notas.append(nota)
+    return notas
+    
